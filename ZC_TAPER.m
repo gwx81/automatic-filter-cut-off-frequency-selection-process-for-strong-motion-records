e94036b2e9b71543ecb8d82cbb7349159268e407
Guo Wenxuan <156687729+gwx81@users.noreply.github.com>
2024-12-22 21:17:41 +0800

    Add files via upload

---
+++ ZC_TAPER.m
@@ -0,0 +1,26 @@
+function [ACC]=ZC_TAPER(acc,DT)
+TB=0;
+TE=0;
+for ii=2:length(acc)
+    if acc(ii,1)*acc(ii-1,1)<0 && ii>=5
+        TB=ii*DT;
+        break
+    end
+end
+if ii==length(acc)
+    TB=0;
+    acc(1,1)=0;
+end
+for ii=length(acc):-1:2
+    if acc(ii,1)*acc(ii-1,1)<0 && ii<=length(acc)-5
+        TE=(length(acc)-ii+1)*DT;
+        break
+    end
+end
+if ii==2
+    TE=0;
+    acc(length(acc),1)=0;
+end
+ACC(:,1)=Taper(DT,acc,TB,TE);
+
+end
